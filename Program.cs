b08d6c81c867d17131d0797677c471a0306da08f
Ольга <141999010+olga1234lanina@users.noreply.github.com>
2023-09-12 21:41:10 +0500

    Update Program.cs
    решение

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// задача 34 . задайте массив заполненный случайными положительными трехзначными числами .
+// напишите программу, которая покажет количество чисел в массиве [345, 897, 234] -> 2
+
+Console.Clear();
+int[] array = GetArray();
+PrintArray(array);
+Console.Write($" -> {GetEvenNumbers(array)}");
+
+int[] GetArray()
+{
+    int size = new Random().Next(5,15);
+    int [] array = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        array [i] = new Random().Next(99, 999);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"[{String.Join(", ", array)}]");
+}
+int GetEvenNumbers(int[] array)
+{
+    int cout = 0;
+    for (int i =0, i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            cout++;
+        }
+    }
+    return cout;
+}
